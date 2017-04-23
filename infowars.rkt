b9e9b5493c9c1cca275ccc32308003237d71f2c2
Dave DaCosta <dacostadavid94@gmail.com>
2017-04-23 16:02:14 -0400

    Add files via upload

---
+++ infowars.rkt
@@ -0,0 +1,49 @@
+#lang racket
+(require web-server/servlet
+         web-server/servlet-env
+         "get-url-return-news.rkt")
+
+(provide infowars)
+
+(define (mypage req)
+  (response/xexpr
+   `(html (head (title "UML INFOWARS II : Faker News!")
+                (link ((rel "stylesheet")
+                       (href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css")
+                       (type "text/css")))
+                (link ((rel "stylesheet")
+                       (href "/webscrape.css")
+                       (type "text/css")))
+                (script ((src "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js")))
+                (script ((src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"))))
+          (body
+           (header 
+                   (div ((class "container"))
+                        (div ((class "row"))
+                             (div ((class "col-lg-12"))
+                                       (h1 ((class "sitename"))
+                                           "UML INFO WARS")
+                                       (h3 ((class "quote"))
+                                              " Democracy is but Mob Rule ") ) ) ) )
+                     
+           (hr ((class "line")))
+           (div ((class "container"))
+                (div ((class "row"))
+                     (div ((class "col-md-6 col-centered"))
+                          (div ((class "text-centered"))
+                               (h3 "Real News"))
+                          ,mynews )
+                     (div ((class "col-md-6 col-centered"))
+                          (div ((class "text-centered"))
+                               (h3 "Fake News"))
+                          ,fakenews  )))   ))) )
+
+(define root (current-directory))
+
+(define z (MarkovModel mynews 12) )
+(define fakenews ((z 'get-the-news) 2000))
+
+(define infowars (serve/servlet mypage
+                       #:extra-files-paths
+                       (list
+                        (build-path root "css"))) )
